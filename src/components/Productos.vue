47ec64a32a2e36f228806e17a33fd96267b85a29
Aweba13 <fiorella.effio@unmsm.edu.pe>
2018-10-24 18:30:47 -0500

    allday better

--- src/components/Productos.vue
+++ src/components/Productos.vue
@@ -26,7 +26,7 @@
               
                           <v-list-tile-content>
                             <v-list-tile-title v-html="product.nombre"></v-list-tile-title>
-                            <v-list-tile-sub-title v-html="product.precio"></v-list-tile-sub-title>
+                            <v-list-tile-sub-title>Precio: {{product.precio}}</v-list-tile-sub-title>
                           </v-list-tile-content>
               
                           <v-list-tile-action>
@@ -43,23 +43,37 @@
               <div v-if="bottomNav === 'allday'">
                 <br><br><br>
                 <br><br><br>
-                <h1>resto del dia</h1>
-                <div v-for="(product, nombre) in allDayProducts" :key="nombre">
-                  <div class="col s12 m6">
-                    <div class="card">
-                      <div class="card-image">
-                        <img width="100px" :src="product.src">
-                        <span class="card-title black-text">{{product.nombre}}</span>
-                      </div>
-                      <div class="card-content">
-                        <p>Precio: S./{{product.precio}}</p>
-                      </div>
-                      <div class="card-action">
-                        <button @click="passToOrder(product)">Agregar al carrito</button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <v-layout row>
+                  <v-flex xs12 sm6 offset-sm3>
+                    <v-card>
+                      <v-toolbar color="teal" dark>
+                        <v-toolbar-title class="text-xs-center">Resto del día</v-toolbar-title>
+                      </v-toolbar>
+                      <v-list two-line subheader>
+                        <v-subheader>De 11:00am a 12:00pm</v-subheader>
+                        <v-list-tile
+                          v-for="(product, nombre) in allDayProducts" :key="nombre"
+                          avatar
+                          @click=""
+                        >
+                          <v-list-tile-avatar>
+                            <img :src="product.src">
+                          </v-list-tile-avatar>
+              
+                          <v-list-tile-content>
+                            <v-list-tile-title v-html="product.nombre"></v-list-tile-title>
+                            <v-list-tile-sub-title>Precio: {{product.precio}}</v-list-tile-sub-title>
+                          </v-list-tile-content>
+              
+                          <v-list-tile-action>
+                            <button @click="passToOrder(product)"><v-icon color="teal">add_shopping_cart</v-icon></button>
+                          </v-list-tile-action>
+                        </v-list-tile>
+                      </v-list>
+                    </v-card>
+                  </v-flex>
+                </v-layout>
+                
               
               </div>
             </div>
